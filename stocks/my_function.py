ffe895e837d51fcf3e676e8a5f3c0c8aa50671ab
yoshikazukakehashi <kakehashi.y2788@gmail.com>
2023-01-10 22:21:59 +0900

    my_function.pyを作成し、自作関数とviews.pyの関数を分けることにした

---
+++ stocks/my_function.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import pandas as pd
+
+
+# 東証から銘柄一覧を取得して、dataフォルダに格納する一連の関数
+def save_file(path, filename, data, mode):
+    # ファイルを保存するためのディレクトリを作成
+    os.makedirs(path, exist_ok=True)
+
+    # ファイルパスを生成
+    file_path = os.path.join(path, filename)
+
+    # 指定したフォルダに保存
+    with open(file_path, mode) as f:
+        f.write(data.content)
+
+
+def brand_xls2csv():
+    read_file = pd.read_excel("/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data/data_all_brand.xls")
+    read_file.to_csv("/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data/data_all_brand.csv", index=True,
+                     header=True)
+
+
+def get_all_brand():
+    req = requests.get("https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls")
+
+    # 保存先ディレクトリのパス
+    new_dir = "/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data"
+
+    # 保存するファイル名
+    filename = "data_all_brand.xls"
+
+    # ファイルを保存する
+    save_file(new_dir, filename, req, "wb")
+
+    brand_xls2csv()
--- stocks/views.py
+++ stocks/views.py
@@ -2,41 +2,10 @@ from django.shortcuts import render
 import requests
 import os
 import pandas as pd
+import my_function
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-# 東証から銘柄一覧を取得して、dataフォルダに格納する一連の関数
-def save_file(path, filename, data, mode):
-    # ファイルを保存するためのディレクトリを作成
-    os.makedirs(path, exist_ok=True)
-
-    # ファイルパスを生成
-    file_path = os.path.join(path, filename)
-
-    # 指定したフォルダに保存
-    with open(file_path, mode) as f:
-        f.write(data.content)
-
-
-def brand_xls2csv():
-    read_file = pd.read_excel("/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data/data_all_brand.xls")
-    read_file.to_csv("/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data/data_all_brand.csv", index=True,
-                     header=True)
-
-
-def get_all_brand():
-    req = requests.get("https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls")
-
-    # 保存先ディレクトリのパス
-    new_dir = "/Users/yoshikazukakehashi/PycharmProjects/kabu_django2/data"
-
-    # 保存するファイル名
-    fname = "data_all_brand.xls"
-
-    # ファイルを保存する
-    save_file(new_dir, fname, req, "wb")
-
-    brand_xls2csv()
